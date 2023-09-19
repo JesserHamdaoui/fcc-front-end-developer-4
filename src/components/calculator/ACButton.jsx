cb963c31ad2c16ae9b0e6c019ba55731682452e4
JesserHamdaoui <107654958+JesserHamdaoui@users.noreply.github.com>
2023-09-19 10:32:36 +0100

    ADD CALCULATOR COMPONENTS
    Create the calculator main components with some sub components
    Include the calculator component in the app component

---
+++ src/components/calculator/ACButton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ACButton = () => {
+  return <div></div>;
+};
+
+export default ACButton;
---
+++ src/components/calculator/OppirationButton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OppirationButton = () => {
+  return <div></div>;
+};
+
+export default OppirationButton;
